3d07821537392385916f75a68543f0097ce72b79
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2020-01-05 00:23:44 +0200

    docs: Mock 'dotenv'
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,7 @@ release = satnogsconfig.__version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+autodoc_mock_imports = ['dotenv']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
