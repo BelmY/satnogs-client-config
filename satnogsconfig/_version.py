2f4bc1bc58f066340059eb2fed07d159ba5b2b43
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2020-01-05 19:18:55 +0200

    versioneer: Fix path to version file
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- satnogsconfig/_version.py
+++ satnogsconfig/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = ""
-    cfg.versionfile_source = "satnogssetup/_version.py"
+    cfg.versionfile_source = "satnogsconfig/_version.py"
     cfg.verbose = False
     return cfg
 
